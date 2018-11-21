e0d152657ef985baa2e84140995eda7ed3510927
xiandavis <xiandavis@yahoo.com>
2018-11-21 15:42:54 -0800

    Input accessory views and IBDesignable

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    
+    @IBOutlet weak var wageTxt: CurrencyTextField!
+    @IBOutlet weak var priceTxt: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60)) //  typically use UIButton with frame like this when creating a button programmatically
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1) // type color literal to access swatch
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside) // a selector is a func that will be called at a later point in time. It is using objective-C runtime (feature of objective-C messaging, sending strings btn classes that can call a func. NOT A SWIFT THING)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Mark deletes boilerplate didReceiveMemoryWarning()
+    
+    @objc func calculate() { // @objc added by compiler after clicking Fix button. Earlier versions of Swift did this under the hood automatically, but Swift 4 wants you to know when a func is an obj-c runtime func
+        
+        print("We got here")
     }
+
 
 
 }
--- window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/Views/CurrencyTextField.swift
@@ -8,24 +8,34 @@
 
 import UIKit
 
+@IBDesignable // called by IB. Allows below modifications to show up BEFORE runtime
 class CurrencyTextField: UITextField { // blue words are properties of this class!
+    
+    override func prepareForInterfaceBuilder() { // @IBDesignable above requires this func
+        customizeView()
+    }
 
-    override func awakeFromNib() {
+    override func awakeFromNib() { // called at Runtime
         super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() { // Mark made this func name up and moved below code out of awakeFromNib() above so that it could be called from both prepareForInterfaceBuilder() AND from awakeFromNib()
+        
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25) // type 'color literal', set to White @ 25% opacity. backgroundColor is a property of UITextField
         layer.cornerRadius = 5.0 // layer is a property of UITextField (hence blue text color)
         textAlignment = .center // textAlignment is a property of UITextField
         
         /*
-        if placeholder == nil {
-            placeholder = " " // initialization/safety check for force unwrapped optional [placeholder!] below. placeholder is a property of UITextField. Mark added a space in string so code would run
-        }
+         if placeholder == nil {
+         placeholder = " " // initialization/safety check for force unwrapped optional [placeholder!] below. placeholder is a property of UITextField. Mark added a space in string so code would run
+         }
+         
+         let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+         attributedPlaceholder = place // if this is set, it will override the regular placeholder above. attributedPlaceholder is a property of UITextField
+         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+         */
         
-        let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
-        attributedPlaceholder = place // if this is set, it will override the regular placeholder above. attributedPlaceholder is a property of UITextField
-        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        */
- 
         if let p = placeholder { // if placeholder is not nil, assign to p. Renders above [folded] statements obsolete
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place // if this is set, it will override the regular placeholder above. attributedPlaceholder is a property of UITextField
