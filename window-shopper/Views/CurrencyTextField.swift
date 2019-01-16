ee19ff83c131340e3d258d2f9fcd89e25728ecd9
xiandavis <xiandavis@yahoo.com>
2019-01-16 12:14:06 -0800

    draw(CGRECT)

--- window-shopper/Views/CurrencyTextField.swift
+++ window-shopper/Views/CurrencyTextField.swift
@@ -11,6 +11,21 @@ import UIKit
 @IBDesignable // called by IB. Allows below modifications to show up BEFORE runtime
 class CurrencyTextField: UITextField { // blue words are properties of this class!
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20 // Mark specifies float to avoid automatically being set as a Double
+        let currencyLbl = UILabel(frame: CGRect(x: 10, y: (frame.size.height / 2) - size / 2, width: size, height: size)) // Mark used a value of 5 for x, but I think an x value of 10 looks better.
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.9367641135, green: 0.9367641135, blue: 0.9367641135, alpha: 0.8)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true // Mark illustrates that the radiused corners of these [currency icon] rectangles do not display without this statement, as draw(CGRect) removes the behavior. He adds a similar statement inside customizeView() below.
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current // .locale gets the country your device is setup for
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() { // @IBDesignable above requires this func
         customizeView()
     }
@@ -25,6 +40,8 @@ class CurrencyTextField: UITextField { // blue words are properties of this clas
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25) // type 'color literal', set to White @ 25% opacity. backgroundColor is a property of UITextField
         layer.cornerRadius = 5.0 // layer is a property of UITextField (hence blue text color)
         textAlignment = .center // textAlignment is a property of UITextField
+        
+        clipsToBounds = true // Overrides draw(CGRect) allowing radiused corners of text fields to display. See similar statement inside draw(CGRect) above
         
         /*
          if placeholder == nil {
